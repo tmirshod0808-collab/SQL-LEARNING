a826e4e5cf0b48baf815162d5482995844b89524
tmirshod0808-collab <tmirshod0808@gmail.com>
2025-10-14 13:50:25 +0500

    Create hw12.sql

---
+++ lesson-12/homework/hw12.sql
@@ -0,0 +1,209 @@
+
+Create table Person (personId int, firstName varchar(255), lastName varchar(255))
+Create table Address (addressId int, personId int, city varchar(255), state varchar(255))
+Truncate table Person
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen')
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
+Truncate table Address
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York')
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California')
+
+select * from Person
+select * from Address
+
+SELECT 
+    p.firstName, 
+    p.lastName, 
+    a.city, 
+    a.state
+FROM Person p
+LEFT JOIN Address a
+    ON p.personId = a.personId;
+
+	--2
+	Create table Employee (id int, name varchar(255), salary int, managerId int)
+Truncate table Employee
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL)
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL)
+
+
+
+	SELECT 
+    e1.name AS Employee
+FROM Employee e1
+JOIN Employee e2 
+    ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
+
+
+--3
+SELECT 
+    email AS Email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
+
+
+
+--4
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM Person
+    GROUP BY email)
+
+	--5
+	CREATE TABLE boys (
+    Id INT PRIMARY KEY,
+    name VARCHAR(100),
+    ParentName VARCHAR(100)
+);
+
+CREATE TABLE girls (
+    Id INT PRIMARY KEY,
+    name VARCHAR(100),
+    ParentName VARCHAR(100)
+);
+
+INSERT INTO boys (Id, name, ParentName) 
+VALUES 
+(1, 'John', 'Michael'),  
+(2, 'David', 'James'),   
+(3, 'Alex', 'Robert'),   
+(4, 'Luke', 'Michael'),  
+(5, 'Ethan', 'David'),    
+(6, 'Mason', 'George');  
+
+
+INSERT INTO girls (Id, name, ParentName) 
+VALUES 
+(1, 'Emma', 'Mike'),  
+(2, 'Olivia', 'James'),  
+(3, 'Ava', 'Robert'),    
+(4, 'Sophia', 'Mike'),  
+(5, 'Mia', 'John'),      
+(6, 'Isabella', 'Emily'),
+(7, 'Charlotte', 'George');
+
+
+SELECT DISTINCT g.ParentName
+FROM girls g
+WHERE g.ParentName NOT IN (
+    SELECT DISTINCT b.ParentName 
+    FROM boys b
+);
+
+----6
+--6. Total over 50 and least
+--Find total Sales amount for the orders which weights more than 50 for each customer along with their least weight.(from TSQL2012 database, Sales.Orders Table)
+
+--You can find tsql2012 database in here: https://gist.github.com/alejotima/cac70484db23834591b142ad07e79e67
+
+
+SELECT 
+    CustomerID,
+    SUM(SalesAmount) AS TotalSalesOver50,
+    MIN(Weight) AS LeastWeight
+FROM Sales.Orders
+WHERE Weight > 50
+GROUP BY CustomerID;
+
+
+
+
+
+--7
+DROP TABLE IF EXISTS Cart1;
+DROP TABLE IF EXISTS Cart2;
+GO
+
+CREATE TABLE Cart1
+(
+Item  VARCHAR(100) PRIMARY KEY
+);
+GO
+
+CREATE TABLE Cart2
+(
+Item  VARCHAR(100) PRIMARY KEY
+);
+GO
+
+INSERT INTO Cart1 (Item) VALUES
+('Sugar'),('Bread'),('Juice'),('Soda'),('Flour');
+GO
+
+INSERT INTO Cart2 (Item) VALUES
+('Sugar'),('Bread'),('Butter'),('Cheese'),('Fruit');
+GO
+
+
+
+SELECT 
+    c1.Item AS [Item Cart 1],
+    c2.Item AS [Item Cart 2]
+FROM Cart1 c1
+FULL OUTER JOIN Cart2 c2
+    ON c1.Item = c2.Item;
+
+	--8
+
+	Create table Customers (id int, name varchar(255))
+Create table Orders (id int, customerId int)
+Truncate table Customers
+insert into Customers (id, name) values ('1', 'Joe')
+insert into Customers (id, name) values ('2', 'Henry')
+insert into Customers (id, name) values ('3', 'Sam')
+insert into Customers (id, name) values ('4', 'Max')
+Truncate table Orders
+insert into Orders (id, customerId) values ('1', '3')
+insert into Orders (id, customerId) values ('2', '1')
+
+SELECT 
+    c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+    ON c.id = o.customerId
+WHERE o.customerId IS NULL;
+
+--9
+Create table Students (student_id int, student_name varchar(20))
+Create table Subjects (subject_name varchar(20))
+Create table Examinations (student_id int, subject_name varchar(20))
+Truncate table Students
+insert into Students (student_id, student_name) values ('1', 'Alice')
+insert into Students (student_id, student_name) values ('2', 'Bob')
+insert into Students (student_id, student_name) values ('13', 'John')
+insert into Students (student_id, student_name) values ('6', 'Alex')
+Truncate table Subjects
+insert into Subjects (subject_name) values ('Math')
+insert into Subjects (subject_name) values ('Physics')
+insert into Subjects (subject_name) values ('Programming')
+Truncate table Examinations
+insert into Examinations (student_id, subject_name) values ('1', 'Math')
+insert into Examinations (student_id, subject_name) values ('1', 'Physics')
+insert into Examinations (student_id, subject_name) values ('1', 'Programming')
+insert into Examinations (student_id, subject_name) values ('2', 'Programming')
+insert into Examinations (student_id, subject_name) values ('1', 'Physics')
+insert into Examinations (student_id, subject_name) values ('1', 'Math')
+insert into Examinations (student_id, subject_name) values ('13', 'Math')
+insert into Examinations (student_id, subject_name) values ('13', 'Programming')
+insert into Examinations (student_id, subject_name) values ('13', 'Physics')
+insert into Examinations (student_id, subject_name) values ('2', 'Math')
+insert into Examinations (student_id, subject_name) values ('1', 'Math')
+
+
+SELECT 
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+    ON s.student_id = e.student_id 
+   AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
